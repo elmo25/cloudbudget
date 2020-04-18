334c6bb6b0ff48c69410f2ff3e0edd24d821cd78
elmo25 <43646627+elmo25@users.noreply.github.com>
2020-04-18 12:54:55 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var swiper = new Swiper(".swiper-container", {
+    effect: "flip",
+    grabCursor: true,
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+});
